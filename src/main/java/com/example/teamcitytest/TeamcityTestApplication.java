e962f81726b5a7aeb28d07688afc5ad4b5cec87c
waterdragon <tndyd5390@naver.com>
2021-11-17 17:07:47 +0900

    first-commit

---
+++ src/main/java/com/example/teamcitytest/TeamcityTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamcitytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcityTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TeamcityTestApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/teamcitytest/TeamcityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamcitytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamcityTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
